893611576318d93ff086f5c304f0c7a92dff4e8b
emilypier <piersoee@miamioh.edu>
2022-04-27 20:41:02 -0400

    made footer stuck to bottom of page

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,8 +4,8 @@ function Footer(){
   return(
     <footer>
       <a href="mailto:piersoee@gmail.com">email me</a>
-      <a href="https://github.com/emilypier">my GitHub</a>
-
+      <a href="https://github.com/emilypier">GitHub</a>
+      <a href="https://www.linkedin.com/in/emilypierson/">LinkedIn</a>
     </footer>
   )
 }
